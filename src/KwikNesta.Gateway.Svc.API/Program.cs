22ca699cd15dffa9bf763e676c6132c5034c2269
Toba Ojo <ojotobar@gmail.com>
2025-10-06 01:20:37 +0100

    Serilog selflog

--- src/KwikNesta.Gateway.Svc.API/Program.cs
+++ src/KwikNesta.Gateway.Svc.API/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddControllers(o =>
 builder.Services
     .RegisterServices(builder.Configuration, builder.Environment.ApplicationName);
 builder.Host.ConfigureSerilogESSink();
+Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine($"SERILOG: {msg}"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
